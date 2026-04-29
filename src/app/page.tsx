61f938250e38a781ec76084c20ec099a36b15d3b
Sahara <lemonmugi@gmail.com>
2026-04-29 19:46:13 +0300

    add: shadcn/ui and eslint + alias

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/shared/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      hello next
+      <Button variant="outline">Button</Button>
+    </div>
+  );
+}
---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+import fontFamily from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
