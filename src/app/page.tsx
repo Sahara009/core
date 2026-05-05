0388684d88e7d6a21777f0ef6adc6c1e5bbc40bf
Sahara <lemonmugi@gmail.com>
2026-05-05 23:06:49 +0300

    add prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { prisma } from "@/shared/lib/prisma";
 import { Button } from "@/shared/ui/button";
 
-export default function Home() {
+export default async function Home() {
+  const courses = await prisma.course.findMany();
+  console.log(courses);
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       hello next
